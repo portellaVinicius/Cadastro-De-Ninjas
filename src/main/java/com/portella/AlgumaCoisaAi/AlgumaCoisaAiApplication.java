808cc611118e3b352c93dfe1fb24cd645ab45d30
Vinicius <portellavinicius13@gmail.com>
2026-03-02 19:30:36 -0300

    first commit

---
+++ src/main/java/com/portella/AlgumaCoisaAi/AlgumaCoisaAiApplication.java
@@ -0,0 +1,13 @@
+package com.portella.AlgumaCoisaAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgumaCoisaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgumaCoisaAiApplication.class, args);
+	}
+
+}
